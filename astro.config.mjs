42c9a9241375e1614e8a8d75f222d028ccb18f56
lynnjiang <lynnjinjie@gmail.com>
2023-09-08 01:57:21 +0800

    update: posts style change&add dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.geekjjj.me',
-  integrations: [sitemap()],
+  integrations: [sitemap(), tailwind(), react()],
   markdown: {
     syntaxHighlight: 'shiki',
     shikiConfig: {
       theme: 'vitesse-dark',
       wrap: true
     }
-  },
+  }
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,7 @@ const schema = z.object({
 	title: z.string(),
 	description: z.string(),
 	// Transform string to Date object
-	pubDate: z.coerce.date().optional(),
+	pubDate: z.coerce.date(),
 	updatedDate: z.coerce.date().optional(),
 	heroImage: z.string().optional(),
 })
